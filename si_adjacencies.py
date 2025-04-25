8dbee12cdc5a5099718dc25f583a2ccea6d93b31
Tal Shorer <tal.shorer@gmail.com>
2025-04-25 14:45:32 +0300

    si_adjacencies: enumize board layout
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- si_adjacencies.py
+++ si_adjacencies.py
@@ -1,15 +1,20 @@
 from __future__ import annotations
 
-# from dataclasses import dataclass
-# @dataclass
-# class Land:
-#     pass
+import dataclasses
+import enum
+from typing import List
+
+
+@dataclasses.dataclass
+class LayoutEdge:
+    lands: list[int]
+    boundaries: list[int]
 
 
 class Edge:
-    def __init__(self, lands: list[int], boundaries: list[int], parent: Board):
-        self.lands = tuple(lands[::-1])
-        self.boundaries = (0,) + tuple(sorted(boundaries)) + (7,)
+    def __init__(self, layout: LayoutEdge, parent: Board):
+        self.lands = tuple(layout.lands[::-1])
+        self.boundaries = (0,) + tuple(sorted(layout.boundaries)) + (7,)
         self.parent = parent
         self.neighbor = None
 
@@ -36,21 +41,43 @@ class Edge:
             else:
                 self_pos += 1
 
+    def __iand__(self, neighbor):
+        # sorry for really abusing dunders
+        assert self.neighbor is None
+        assert neighbor.neighbor is None
+        self.neighbor = neighbor
+        neighbor.neighbor = self
 
-class Board:
-    def __init__(self, name, letter):
+        return self
+
+
+class Layout(enum.Enum):
+
+    A = ([1, 6, 8, 7, 5, 4, 3], [5, 3, None, 4, None, 6, 5, 3], "2456,34,4,5,678,8,8")
+    B = ([1, 6, 8, 7, 4, 3], [6, 3, None, 4, None, 6, 4], "2456,34,4,57,67,78,8")
+    C = ([1, 6, 8, 7, 4, 3], [5, 3, None, 3, 1, None, 3], "256,345,4,57,67,78,8")
+    D = ([1, 8, 7, 6, 4, 3], [1, None, 4, 1, None, 5, 2], "2578,345,4,56,67,7,8")
+    E = ([1, 7, 8, 6, 4, 3], [4, 2, None, 2, None, 6, 3], "257,35,45,567,7,78,8")
+    F = ([1, 6, 8, 7, 4, 3], [4, 1, None, 3, None, 5, 2], "256,345,4,578,68,8,8")
+    G = ([1, 6, 8, 7, 4, 3], [4, 2, None, 3, None, 6, 4], "26,3456,4,57,678,8,8")
+    H = ([1, 8, 7, 4, 3], [3, None, 5, 1, None, 3], "268,356,45,57,67,78,8")
+
+    def __init__(
+        self,
+        lands: List[int],
+        boundaries: List[int],
+        internal_adjacencies: str,
+    ):
         """
         Lands around the edge in clockwise order starting from 1.
         Boundaries in the same order, None for corners.
         """
-        lands, boundaries, internal_adjacencies = GAME_BOARDS[letter]
-        self.name = name
 
         c1, c2 = [i for i, x in enumerate(boundaries) if x is None]
         self.edges = [
-            Edge(lands[: c1 + 1], boundaries[:c1], self),
-            Edge(lands[c1:c2], boundaries[c1 + 1 : c2], self),
-            Edge(lands[c2 - 1 :], boundaries[c2 + 1 :], self),
+            LayoutEdge(lands[: c1 + 1], boundaries[:c1]),
+            LayoutEdge(lands[c1:c2], boundaries[c1 + 1 : c2]),
+            LayoutEdge(lands[c2 - 1 :], boundaries[c2 + 1 :]),
         ]
         self.internal_adjacencies = {i + 1: set() for i in range(8)}
         for i, val in enumerate(internal_adjacencies.split(",")):
@@ -68,11 +95,19 @@ class Board:
             case "8":
                 return self.edges[0].lands[-1]
 
-    ##    def __getitem__(self, ix):
-    ##        return self.edges[ix]
+
+class Board:
+    def __init__(
+        self,
+        name: str,
+        layout: Layout,
+    ):
+        self.name = name
+        self.layout = layout
+        self.edges = [Edge(edge, self) for edge in layout.edges]
 
     def adjacent(self, land):
-        for i in self.internal_adjacencies[land]:
+        for i in self.layout.internal_adjacencies[land]:
             yield (self, i)
 
         yield from (
@@ -84,27 +119,6 @@ class Board:
 
         # yield the corner adjacencies
 
-    def __iand__(self, neighbor):
-        # sorry for really abusing dunders
-        assert self.neighbor is None
-        assert neighbor.neighbor is None
-        self.neighbor = neighbor
-        neighbor.neighbor = self
-
-        return self
-
-
-GAME_BOARDS = {
-    "A": ((1, 6, 8, 7, 5, 4, 3), (5, 3, None, 4, None, 6, 5, 3), "2456,34,4,5,678,8,8"),
-    "B": ((1, 6, 8, 7, 4, 3), (6, 3, None, 4, None, 6, 4), "2456,34,4,57,67,78,8"),
-    "C": ((1, 6, 8, 7, 4, 3), (5, 3, None, 3, 1, None, 3), "256,345,4,57,67,78,8"),
-    "D": ((1, 8, 7, 6, 4, 3), (1, None, 4, 1, None, 5, 2), "2578,345,4,56,67,7,8"),
-    "E": ((1, 7, 8, 6, 4, 3), (4, 2, None, 2, None, 6, 3), "257,35,45,567,7,78,8"),
-    "F": ((1, 6, 8, 7, 4, 3), (4, 1, None, 3, None, 5, 2), "256,345,4,578,68,8,8"),
-    "G": ((1, 6, 8, 7, 4, 3), (4, 2, None, 3, None, 6, 4), "26,3456,4,57,678,8,8"),
-    "H": ((1, 8, 7, 4, 3), (3, None, 5, 1, None, 3), "268,356,45,57,67,78,8"),
-}
-
 
 if __name__ == "__main__":
 
@@ -112,7 +126,9 @@ if __name__ == "__main__":
         for i, j in edge1 @ edge2:
             print(f"{i} {j}")
 
-    p, q, r = Board("🐑P", "H"), Board("🐑Q", "A"), Board("🐑R", "D")
+    p = Board("🐑P", Layout.H)
+    q = Board("🐑Q", Layout.A)
+    r = Board("🐑R", Layout.D)
 
     p.edges[1] &= q.edges[2]
     q.edges[1] &= r.edges[2]
